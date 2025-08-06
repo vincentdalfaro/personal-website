ee586c455a70ad2fbcd9fef76a631b61b13b7937
vincentdalfaro <vdalfaro20@gmail.com>
2025-08-06 12:29:42 -0700

    Cleaning up framework

---
+++ src/HomePage.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const HomePage = () => {
+
+  return (
+    <div>
+        Hello, you've wandered into the greay abyss. Welcome.
+    </div>
+  );
+};
+
+export default HomePage;
